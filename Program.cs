5647477472325eb61b947361fd2163dd39a65763
lui <luigi.danho99@gmail.com>
2026-05-17 20:39:48 +0200

    My first fitnesstracker.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hallo zu meinem Fitnesstracker");
+        
+        Console.WriteLine("Welchen Sport hast du gemacht?");
+        string? sportart = Console.ReadLine();
+        Console.WriteLine("Du hast geantwortet: " + sportart);
+    
+        Console.WriteLine("Wie viele Minuten hast du trainiert?");
+        string? dauerText = Console.ReadLine();
+        int dauer = int.Parse(dauerText!);
+        Console.WriteLine("Super! Das sind  " + dauer + " Minuten " + sportart + ".");
+
+    
+    }
+}
